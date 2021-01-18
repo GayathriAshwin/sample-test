95ed0b66341403afdc5eb0469894134301e430fe
GayathriAshwin <gayathri.b123@gmail.com>
2021-01-18 16:01:29 +0530

    Create sample_add.sh

---
+++ example/sample_add.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+msg=$1
+
+security_header=`/opt/python-2.6.4/bin/python /0config/create_security_header.py`
+
+url="https://IBMWorkloadDeployer:9443/services/deployments/${DEPLOYMENT_ID}/command"
+
+cmd={\"operation\":\"addHistory\",\"history_message\":\""${msg}"\"}
+
+curl -X POST -H "X-IWD-Authorization : $security_header" -H "Content-Type: application/json" --cacert /0config/cert.tmp/cert_1.pem  -d "${cmd}"  --url "$url"
